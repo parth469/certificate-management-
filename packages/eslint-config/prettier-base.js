704807206038bba98ac198719fdb022084f60842
parth469 <parthmain2001@gmail.com>
2025-05-14 11:52:31 +0530

    impove nestjs fix

---
+++ packages/eslint-config/prettier-base.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    "singleQuote": true,
+    "trailingComma": "all",
+    "endOfLine": "lf",
+    "tabWidth": 2,
+    "semi": true,
+    "printWidth": 100
+}
