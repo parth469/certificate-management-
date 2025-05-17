960207fc4de478c991cfa6e71bee4b8d5e5d8e30
parth469 <parthmain2001@gmail.com>
2025-05-17 22:44:57 +0530

    - cors & version setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    }
+
+  });
+
+  app.enableVersioning({
+    defaultVersion: '0',
+    type: VersioningType.URI
+  })
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
